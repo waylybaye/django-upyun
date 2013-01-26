e6053956df20250ba95b6896f237d2a8a9bb5088
waylybaye <havelove@gmail.com>
2013-01-26 21:28:06 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+django-upyun
+=============
+
+django storage for [又拍云存储](http://upyun.com)
---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = __import__('django_upyun').__version__
+
+setup(
+    name="django-upyun",
+    packages=['django_upyun'],
+    version=version,
+    author="Baye Wayly",
+    author_email="havelove@gmail.com",
+    url="https://github.com/waylybaye/django-upyun",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)
